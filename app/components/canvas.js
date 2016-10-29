8c17bff30a255c5c3a607ec60cafa08bcd7a3be6
lau-ren <colls.lauren@gmail.com>
2016-10-29 13:57:39 +1300

    added webviewbridge

--- app/components/canvas.js
+++ app/components/canvas.js
@@ -6,11 +6,28 @@
 import Orientation from 'react-native-orientation'
 import React, { Component } from 'react';
 import { View, WebView } from 'react-native';
+import WebViewBridge from 'react-native-webview-bridge';
 
+const injected = `
+  (function () {
+          console.log(WebViewBridge, "webview bridge")
+          WebViewBridge.onMessage = function (reactNativeData) {
+            WebViewBridge.send("got the message")
+            // var jsonData = JSON.parse(reactNativeData);
+            // WebViewBridge.send(dataToSend);
+          };
+      }())
+`
 
 export default class Canvas extends Component {
   componentDidMount () {
     Orientation.lockToLandscapeLeft()
+
+    this.refs.webviewbridge.sendToBridge('Hello World')
+  }
+
+  onBridgeMessage(webviewData) {
+    Alert.alert(webviewData);
   }
 
 
@@ -18,20 +35,18 @@ export default class Canvas extends Component {
   render() {
     return (
       <View>
-        <WebView
+        <WebViewBridge
           source={ require('./canvas.html') }
+          ref="webviewbridge"
+          onBridgeMessage ={this.onBridgeMessages}
           scalesPageToFit={ true }
+          injectedJavascript={injected}
           style={ {
             width: 650,
             backgroundColor: 'blue'
           } }
         />
       </View>
-    );
+    )
   }
 }
-
-        // <WebView
-        //   source={{uri: 'https://github.com/facebook/react-native'}}
-        //   style={{marginTop: 20}}
-        // />
