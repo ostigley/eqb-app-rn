c5c3aee26829d6a5e5f387d07fc41ea94b5f47fa
Oliver Stigley <oliverstigley@gmail.com>
2017-01-22 11:23:45 +1300

    test dimensions component created
    - rendering on app homepage
    - loads in background and sends dimensions back to app

---
+++ app/views/test-dimensions.js
@@ -0,0 +1,75 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+import Orientation from 'react-native-orientation'
+import React, { Component } from 'react';
+import { View, Text, ScrollView, Image, Dimensions, PixelRatio } from 'react-native';
+import WebViewBridge from 'react-native-webview-bridge';
+import Clue          from './clue.js'
+
+const injected =`
+(function () {
+  if (WebViewBridge) {
+    WebViewBridge.send(JSON.stringify({action: 'Initiating', data: {}}))
+
+    WebViewBridge.onMessage = function (action) {
+      switch (action) {
+        case 'handshake confirmation please':
+          WebViewBridge.send(JSON.stringify({
+            action: 'Confirming', data: { height: window.innerHeight, width: window.innerWidth }
+          }))
+          break
+      }
+    };
+  }
+}());`
+
+
+export default class TestDimensions extends Component {
+  constructor (props) {
+    super(props)
+  }
+
+  onBridgeMessage(incoming) {
+    message = JSON.parse(incoming)
+    const { webviewbridge } = this.refs;
+    switch (message['action']) {
+      case 'Initiating':
+        console.log('TestDimension WebViewBridge Link initiated')
+        webviewbridge.sendToBridge('handshake confirmation please')
+        break
+      case 'Confirming':
+        console.log('TestDimension Link confirmed', message.data)
+        break
+      case 'canvas data':
+        console.log('canvas data', message.data.length)
+        break
+    }
+  }
+
+  render() {
+    return (
+        <WebViewBridge
+          source={ require('./canvas.html') }
+          ref='webviewbridge'
+          onBridgeMessage = { this.onBridgeMessage.bind(this) }
+          scalesPageToFit = { true }
+          javaScriptEnabled={ true }
+          injectedJavaScript={ injected }
+          style={ styles.webview }
+        />
+    )
+  }
+}
+
+const styles = {
+  webview: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').width,
+    zIndex: 0,
+    opacity: 0,
+
+  }
+}
