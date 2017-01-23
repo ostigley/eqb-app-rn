23cced4b0485b625044b92e317fae81ce19f1180
Oliver Stigley <oliverstigley@gmail.com>
2017-01-23 17:17:25 +1300

    This commit fixes the final image dimensions correctly

--- app/views/final.js
+++ app/views/final.js
@@ -2,31 +2,35 @@ import React, { Component } from 'react';
 import { Image, Text, View, Dimensions } from 'react-native';
 import Orientation from 'react-native-orientation'
 
-const Final = ({finalImage}) => {
+const Final = ({ finalImage }) => {
   Orientation.lockToPortrait()
-	return (
+
+
+  var {width, height} = Dimensions.get('window')
+  width = width > height ? height : Dimensions.get('window').height
+  height = height < width ? width : Dimensions.get('window').width
+
+  const styles = {
+    image: {
+      flex: 1,
+      height: height,
+      width: width,
+      borderWidth: 20
+    },
+    container: {
+      flex: 1
+    }
+  }
+
+  return (
     <View style={ styles.container}>
       <Image
-      	style={ styles.image }
-      	source={ {uri: finalImage } }
-      	resizeMode={'contain'} />
+        style={ styles.image }
+        source={ { uri: finalImage } }
+        resizeMode={ 'contain' } />
     </View>)
 }
 
 export default Final
 
-var {width, height} = Dimensions.get('window')
-width = width > height ? width : Dimensions.get('window').height
-height = height < width ? height : Dimensions.get('window').width
 
-
-const styles = {
-  image: {
-    flex: 1,
-    height: height,
-    width: width
-  },
-  container: {
-    flex: 1
-  }
-}
